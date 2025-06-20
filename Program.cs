d17062987ecb8b65d5f158b08b124c46ffd20d7c
DanielBarbieri21 <dibarbieri21@gmail.com>
2025-06-20 14:04:47 -0300

    Alteração Concluida

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando Nokia:");
+Smartphone nokia = new Nokia("123456", "Nokia C20", "111111111", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n-----------------------------\n");
+
+Console.WriteLine("Testando iPhone:");
+Smartphone iphone = new Iphone("987654", "iPhone 13", "999999999", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
